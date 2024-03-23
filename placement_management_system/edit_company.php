fc11eb3c0ee2c5556fc0f223bd80f1becbcf77f1
Madhumitha R <98893549+madhumitha-ravichandran26@users.noreply.github.com>
2024-03-23 13:31:53 +0530

    Update edit_company.php

--- placement_management_system/edit_company.php
+++ placement_management_system/edit_company.php
@@ -1,11 +1,11 @@
 <?php
 
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "pms";
+$db_hostname="127.0.0.1";
+$db_username="root";
+$db_password="";
+$db_name="pms";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn=mysqli_connect($db_hostname,$db_username,$db_password,$db_name);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
