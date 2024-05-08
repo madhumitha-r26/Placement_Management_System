900810e570a86855216f3b8cd93334af74cf721e
Madhumitha R <98893549+madhumitha-ravichandran26@users.noreply.github.com>
2024-05-08 13:24:39 +0530

    Update view_company.php

--- placement_management_system/view_company.php
+++ placement_management_system/view_company.php
@@ -70,6 +70,35 @@ if(!$result)
         height: auto;
         padding-right: 5px;
     }
+
+      th{
+    background-color:#2b2929;
+    border: 2px black solid;
+    text-align: center;
+    color: beige;
+  }
+
+  td{
+     border: 2px black solid;
+  }
+
+tr:nth-child(even){
+    height:auto;
+    width: 100%;
+    word-wrap: break-word;
+    border: 2px black solid;
+    background-color:rgb(255, 255, 232);
+}
+
+tr:nth-child(odd){
+    height:auto;
+    width: 100%;
+    word-wrap: break-word;
+    border: 2px black solid;
+    background-color:rgb(213, 213, 169);
+}
+
+    
 </style>
 
 </head>
